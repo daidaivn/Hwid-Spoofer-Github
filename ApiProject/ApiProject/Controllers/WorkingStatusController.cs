565db38e26626b34650fbb178d1a8ab94671311c
BinhLe <binh.le100799@gmail.com>
2022-09-15 10:57:02 +0700

    add full workingstatus

---
+++ ApiProject/ApiProject/Controllers/WorkingStatusController.cs
@@ -0,0 +1,88 @@
+﻿using ApiProject.IServices;
+using ApiProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkingStatusController : ControllerBase
+    {
+        private readonly IWorkingStatusService _workingStatusService;
+        public WorkingStatusController(IWorkingStatusService workingStatusService)
+        {
+            _workingStatusService = workingStatusService;
+        }
+
+        [Route("get-all-working-status")]
+        [HttpGet]
+        public dynamic getAllWorkingStatus()
+        {
+            try
+            {
+                var data = _workingStatusService.getAllWorkingStats();
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("creat-ws")]
+        [HttpPost]
+        public dynamic Create(WorkingStatus workingStatus)
+        {
+            try
+            {
+                var Created = _workingStatusService.CreateWorkingStatus(workingStatus);
+                return Ok(Created);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("update-ws")]
+        [HttpPut]
+        public dynamic UpdateWorkingStatus(WorkingStatus workingStatus)
+        {
+            try
+            {
+                var Updated = _workingStatusService.UpdateWorkingStatus(workingStatus);
+                return Ok(Updated);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("search-by-ws-name")]
+        [HttpPost]
+        public dynamic SearchByWorkingStatusName(WorkingStatus workingStatus)
+        {
+            try
+            {
+                var result1 = _workingStatusService.SearchByWorkingStatusName(workingStatus);
+                return Ok(result1);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("search-by-ws-id")]
+        [HttpPost]
+        public dynamic SearchByWorkingStatusId(WorkingStatus workingStatus)
+        {
+            try
+            {
+                var result2 = _workingStatusService.SearchByWorkingStatusId(workingStatus);
+                return Ok(result2);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ ApiProject/ApiProject/IServices/IWorkingStatusServices.cs
@@ -0,0 +1,13 @@
+﻿using ApiProject.Models;
+
+namespace ApiProject.IServices
+{
+    public interface IWorkingStatusService
+    {
+        public IQueryable<dynamic> getAllWorkingStats();
+        public dynamic CreateWorkingStatus(WorkingStatus workingStatus);
+        public dynamic UpdateWorkingStatus(WorkingStatus workingStatus);
+        public IQueryable<dynamic> SearchByWorkingStatusName(WorkingStatus workingStatus);
+        public dynamic SearchByWorkingStatusId(WorkingStatus workingStatus);
+    }
+}
