4435c320bb81654344577b27f3b166e949631ebc
BinhLe <binh.le100799@gmail.com>
2022-09-19 09:09:23 +0700

    add CWcontroll, fix working

--- ApiProject/ApiProject/Controllers/CWController.cs
+++ ApiProject/ApiProject/Controllers/CWController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiProject.IServices;
+using ApiProject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiProject.Controllers
@@ -7,5 +9,52 @@ namespace ApiProject.Controllers
     [ApiController]
     public class CWController : ControllerBase
     {
+        private readonly ICWService _cwservice;
+        public CWController(ICWService cwservice)
+        {
+            _cwservice = cwservice;
+        }
+        [Route("getall-cw")]
+        [HttpGet]
+        public dynamic GetAllCW()
+        {
+            try
+            {
+                var data = _cwservice.GetAllCW();
+                return data;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("update-cw")]
+        [HttpPut]
+        public dynamic UpdateCW(Working working)
+        {
+            try
+            {
+                var data = _cwservice.UpdateCW(working);
+                return data;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        [Route("creat-cw")]
+        [HttpPost]
+        public dynamic CreateCW(Working working)
+        {
+            try
+            {
+                var data = _cwservice.CreateCW(working);
+                return data;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
     }
 }
--- ApiProject/ApiProject/Services/WorkingService .cs
+++ ApiProject/ApiProject/Services/WorkingService .cs
@@ -12,17 +12,38 @@ namespace ApiProject.Services
         }
         public IQueryable<dynamic> getAllWorking()
         {
-            return _context.Workings;
+            var items = _context.Workings;
+            var output = from item in items
+                         select new
+                         {
+                             item.WorkingId,
+                             item.WorkingName,
+                             item.DateCreate,
+                             item.Deadline,
+                             item.Prioritized,
+                             item.Categories,
+                             item.UserConfirm,
+                             item.Description,
+                         };
+            return output;
         }
         public dynamic CreateWorking(Working working)
         {
-            Working rl = new Working
+
+            Working nwork = new Working
             {
                 WorkingName = working.WorkingName,
+                DateCreate = working.DateCreate,
+                Deadline = working.Deadline,
+                Prioritized = working.Prioritized,
+                Categories = working.Categories,
+                UserConfirm = working.UserConfirm,
+                Description = working.Description,
+                WorkingStatusId = working.WorkingStatusId,
             };
-            _context.Workings.Add(rl);
+            _context.Workings.Add(nwork);
             _context.SaveChanges();
-            return rl;
+            return nwork;
         }
         public dynamic UpdateWorking(Working working)
         {
@@ -32,6 +53,14 @@ namespace ApiProject.Services
                 return false;
             }
             checkId.WorkingName = working.WorkingName;
+            checkId.DateCreate = working.DateCreate;
+            checkId.Deadline = working.Deadline;
+            checkId.Prioritized = working.Prioritized;
+            checkId.Categories = working.Categories;
+            checkId.UserConfirm = working.UserConfirm;
+            checkId.Description = working.Description;
+            checkId.WorkingStatusId = working.WorkingStatusId;
+
             _context.Workings.Update(checkId);
             _context.SaveChanges();
             return checkId;
